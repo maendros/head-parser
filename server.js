06e803dc1108b14f689d7afca96ec428e91da245
Paschalis <Valitsianius@hotmail.com>
2016-03-30 07:08:30 +0000

    modifying main file

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var app = express();
 var port=process.env.PORT || process.env.IP;
 
 app.get('/', function(req, res) {
-    if (req.url === '/favicon.ico') return;
+
+    
+    
+
     res.json({
         "Ip Address": req.headers['x-forwarded-for'] ||
                    req.connection.remoteAddress,
         "Language": req.headers["accept-language"].split(",")[0],
         "Software": req.headers["user-agent"].split('(')[1].split(')')[0]
     });
+    
 });
 app.listen(port, function() {
     console.log('Node app is running on port', port);
